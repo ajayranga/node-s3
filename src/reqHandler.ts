7faa0344006c1ac7c2d9e21453b91081f5198442
ajay <ramgaa307@gmail.com>
2024-02-25 14:50:46 +0530

    fetch body

--- src/reqHandler.ts
+++ src/reqHandler.ts
@@ -1,12 +1,32 @@
-import { RequestListener } from "node:http";
-
 const reqHandler = (req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(
-    JSON.stringify({
-      data: "Hello World!",
-    })
-  );
-  return res;
+  const { url, method } = req;
+  if (url === "/file-upload" && method === "POST") {
+    let body = "";
+    req.on("data", (data) => {
+      body += data;
+    });
+    return req.on("end", () => {
+      try {
+        body = JSON.parse(body);
+      } catch (e) {}
+      console.log(body);
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(
+        JSON.stringify({
+          data: "Hello World!",
+        })
+      );
+      return res;
+    });
+  } else {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        data: "Server is running",
+      })
+    );
+    return res;
+  }
 };
+
 export default reqHandler;
